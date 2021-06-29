7a88249a4f8bd0441b260de4b859b2ebdd7a4a2a
m666362 <mdrayhan9464@gmail.com>
2021-06-29 11:21:02 +0600

    object detected yolo weights removed

---
+++ ObjectRecognition/ImageObjectDetection.py
@@ -0,0 +1,68 @@
+import cv2
+import numpy as np
+
+#Load YOLO Algorithm
+net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
+classes = []
+with open("coco.names", "r") as f:
+    classes = [line.strip() for line in f.readlines()]
+layer_names =  net.getLayerNames()
+output_layers = [layer_names[i[0]-1] for i in net.getUnconnectedOutLayers()]
+colors = np.random.uniform(0,255,size=(len(classes), 3))
+
+#load image
+image = cv2.imread("img/object1.jpeg")
+image = cv2.resize(image, None, fx=0.5, fy=0.5)
+height, width, channels = image.shape
+
+#Detect Objects
+blob = cv2.dnn.blobFromImage(image, 0.00392, (416, 416), (0,0,0), True, crop=False)
+
+# for b in blob:
+#     for n, img in enumerate(b):
+#         cv2.imshow(str(n), img)
+
+net.setInput(blob)
+outs = net.forward(output_layers)
+print(outs)
+
+#showing information on the screen
+class_ids = []
+confidences = []
+boxes = []
+for out in outs:
+    for detection in out:
+        scores = detection[5:]
+        class_id = np.argmax(scores)
+        confidence = scores[class_id]
+        if confidence>0.5:
+            center_x = int(detection[0]*width)
+            center_y = int(detection[1]*height)
+            w = int(detection[2]*width)
+            h = int(detection[3]*height)
+            # cv2.circle(image, (center_x, center_y), 10, (0,255,0), 2);
+            x= int(center_x -w/2)
+            y= int(center_y -h/2)
+            #cv2.rectangle(image, (x, y), (x+w, y+h) , (0,255,0), 2)
+            boxes.append([x,y,w,h])
+            confidences.append(float(confidence))
+            class_ids.append(class_id)
+
+indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5,0.4) # threshold value for duplicate detection
+print(indexes)
+detectedObjects = len(boxes)
+font = cv2.FONT_HERSHEY_PLAIN
+for i in range(len(boxes)):
+    if i in indexes:
+        x,y,w,h = boxes[i]
+        label = str(classes[class_ids[i]])
+        color = colors[i]
+        print(label)
+        # cv2.rectangle(image, (x,y), (x+w, y+h), (0,255,0), 2)
+        cv2.rectangle(image, (x, y), (x + w, y + h), color, 2)
+        cv2.putText(image, label, (x, y+30), font, 3, color, 3)
+
+
+cv2.imshow('Image', image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
